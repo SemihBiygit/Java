90a49c1d80da3606fb2dec26d24f40b747fa75c4
Semih Biygit <semihbiygit@gmail.com>
2022-06-28 11:49:29 +0300

    test

---
+++ src/com/bilgeadam/boost/java/lesson042/Test.java
@@ -0,0 +1,12 @@
+package com.bilgeadam.boost.java.lesson042;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("sea");
+		System.out.println("asdasd");
+		System.out.println("asdasd");
+	}
+
+}
