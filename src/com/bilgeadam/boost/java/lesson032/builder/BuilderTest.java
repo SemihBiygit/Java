44a4427d00596ea6f4249a1cc95f8bcfd9d00f39
Semih Biygit <semihbiygit@gmail.com>
2022-06-30 10:05:54 +0300

    lombok and apache stringutils added.

--- src/com/bilgeadam/boost/java/lesson032/builder/BuilderTest.java
+++ src/com/bilgeadam/boost/java/lesson032/builder/BuilderTest.java
@@ -1,6 +1,7 @@
 package com.bilgeadam.boost.java.lesson032.builder;
 
 public class BuilderTest {
+	
 	public static void main(String[] args) {
 		House house = new House.HouseBuilder(200, 4, 2, 20, new Adress("cadde", 95, 16135, "Bursa", "Turkiye"))
 				.areaOfBackGarden(300).areaOfPorch(20).build();
